d5cc5ee64a5cdcba7311c7fc72ef30958b91fa38
bretzel <lussier.serge@gmail.com>
2019-01-04 13:02:07 -0500

    sync; playing with tests...

--- xio.exe/ansi.cxx
+++ xio.exe/ansi.cxx
@@ -102,18 +102,21 @@ void test_method(Ansi::CAnsi& ansi)
     struct met_tester {
 
         std::string function(int a1, const std::string& a2){
-            std::cerr << "a1=" << a1 << ", a2='" << a2 << "'\n";
-            return "test_tester::function...";
+            logdebugpfn << "a1:[" << xio::logger::Yellow << a1 
+                << xio::logger::White << "] a2:["
+                << xio::logger::Yellow << a2 << xio::logger::White << ']' <<  Ends;
+                
+            return "OK";
         }
     };
     met_tester m;
     try {
-    //xio::method<met_tester,int,int, const std::string&> me = xio::method<met_tester,int,int, const std::string&>("function", &m, &met_tester::function);
+    
     xio::method<met_tester,std::string,int, const std::string&> me("function", &m, &met_tester::function);
     std::string r = me(200,"Hey Salutations!");
     std::cerr << __FUNCTION__ << " r = " << r << '\n';
     xio::alu::list_t params(2);
-    params = {xio::alu(200),"Hey Salut La Gang hjahaha!"};
+    params = {xio::alu(200),"xio::method param #2"};
     xio::alu rr = me(params);
     std::cerr << __FUNCTION__ << " rr = " << rr() << '\n';
     }
@@ -138,7 +141,7 @@ auto main(int argc, char **argv) -> int {
     logsuccess << str() << Ends;
     loginfo << "test  _type = 16401: " << xio::type_t::name(16401) << ": " << Ends;
     //test_xio(con);
-    //test_method(con);
+    test_method(con);
     con.execute();
     xio::message::clear([](xio::message& msg){
         lognotice  << msg() << Ends;
