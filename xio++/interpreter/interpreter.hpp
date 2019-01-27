635ff027bb8a88c0b8c6b363bc619664e5723e66
bretzel <lussier.serge@gmail.com>
2019-01-27 15:25:31 -0500

    Sync with msvc/visual studio 2017; Removed function_t.* from this project for the good. Variadic template params both in and out delegate calls from the interpreter.

--- xio++/interpreter/interpreter.hpp
+++ xio++/interpreter/interpreter.hpp
@@ -12,7 +12,7 @@ class xio_api interpreter : public xio_module{
         
 public:
 
-    class rtfc {
+    class xio_api rtfc {
     public:
         using list_t = std::vector<rtfc*>;
     };
@@ -67,42 +67,6 @@ public:
     alu jsr() override;
     
 }; 
-
-// template<typename C, typename R, typename ...A> class methodology{
-//     
-//     std::string _name;
-//     
-//     C* rt_obj = nullptr;
-//     
-//     using rt_fn_t = R(C::*)(A...);
-//     rt_fn_t rt_fn = nullptr;
-//     //using out_fn_t  = alu(const alu::list_t& params);
-//     
-// public:
-//     
-//     methodology(C& a_obj, rt_fn_t fn){
-//         rt_obj = &a_obj;
-//         rt_fn = fn;
-//     }
-//     
-//     std::string& name() { return _name; }
-//     R operator()(const A& ...args){
-//         auto param = [](auto a){
-//             return alu(a);
-//         };
-//         alu::list_t params = { param(args)...};
-//         // alu a = interpreter::enter(_name, params);
-//         // return a.value<R>();
-//         return R();
-//     }
-//     
-//     template <std::size_t ... Is> alu pack (alu::list_t const & params, std::index_sequence<Is...> const &)
-//     { return (rt_obj->*rt_fn)(params[Is].value<A>()...); }
-// 
-//     alu operator()(const std::string& f_name, const alu::list_t& params){        
-//         return pack(params, std::index_sequence_for<A...>{});
-//     }
-// };
 
 
 
