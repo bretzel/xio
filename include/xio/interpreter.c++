172c7f7c699b4402b14e12d845cba20aa1e65274
bretzel <lussier.serge@gmail.com>
2020-01-25 17:51:33 -0500

    Removed base code and lexical analyzer.
    Moved into a separated lib (lus).

---
+++ include/xio/interpreter.c++
@@ -0,0 +1,5 @@
+//
+// Created by bretzel on 20-01-25.
+//
+
+#include "interpreter.h"
---
+++ include/xio/interpreter.h
@@ -0,0 +1,17 @@
+//
+// Created by bretzel on 20-01-25.
+//
+
+#pragma once
+#include <lus/lexical_scanners.h>
+#include <lus/d
+namespace xio
+{
+
+class interpreter : public
+{
+
+};
+
+//#endif //XIO_INTERPRETER_H
+}
